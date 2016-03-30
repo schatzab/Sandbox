27b4c11b0752a36104902252dcc836e3592acf83
Chelsea <runinbend@gmail.com>
2016-03-30 10:49:51 -0700

    new app load

---
+++ test/controllers/header_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HeaderControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/loc_edit_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LocEditTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
